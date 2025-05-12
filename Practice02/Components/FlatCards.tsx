9380adfac79dddc55619101a8fee08120e8710fb
aslamm14 <awais.aslam@netsoltech.com>
2025-05-12 17:29:48 +0500

    create practice 2 folder and prepare practice 2 structure

---
+++ Practice02/Components/FlatCards.tsx
@@ -0,0 +1,14 @@
+// shortcut to add below snippet in empty file rnfs
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FlatCards() {
+  return (
+    <View>
+      <Text>FlatCards</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ Practice02/Src/Practice02.tsx
@@ -0,0 +1,18 @@
+// shortcut to add below snippet in empty file rnfe
+
+import { SafeAreaView, View, Text } from "react-native";
+import React from "react";
+import FlatCards from "../Components/FlatCards";
+
+const Practice02 = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Practice02</Text>
+        <FlatCards />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Practice02;
