61243acc80b292b6a02c8e205c7e05312a1afcf1
aslamm14 <awais.aslam@netsoltech.com>
2025-05-21 16:44:53 +0500

    Setup music player create files and install libraries

---
+++ Practice08/src/practice08.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function practice08() {
+  return (
+    <SafeAreaView>
+      <View style={{ marginLeft: 10 }}>
+        <Text>practice08</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import TrackPlayer from 'react-native-track-player';
 import { AppRegistry } from "react-native";
 import App from "./Practice01/src/AppPro";
 import App2 from "../ReactNative/Practice02/src/Practice02";
@@ -10,6 +10,9 @@ import App4 from "./Practice04/src/practice04";
 import App5 from "./Practice05/src/practice05";
 import App6 from "./Practice06/src/practice06";
 import App7 from "../ReactNative/Practice07/src/practice07";
+import App8 from "../ReactNative/Practice08/src/practice08";
 import { name as appName } from "./app.json";
+import {playbackService} from './musicPlayerService'
 
-AppRegistry.registerComponent(appName, () => App7);
+AppRegistry.registerComponent(appName, () => App8);
+TrackPlayer.registerPlaybackService(() => playbackService);
